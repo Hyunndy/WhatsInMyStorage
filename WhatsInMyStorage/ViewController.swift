6e40346d4bc49ea4152468e6eaf76d2f65aab5ed
Hyunndy <yoosa3004@naver.com>
2022-10-31 00:47:36 +0900

    Initial Commit

---
+++ WhatsInMyStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsInMyStorage
+//
+//  Created by hyunndy on 2022/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
