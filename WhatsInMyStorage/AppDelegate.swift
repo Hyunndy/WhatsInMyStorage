c944e023da11c2deefef2b803caef1b6dcf91832
Hyunndy <yoosa3004@naver.com>
2022-11-01 02:55:25 +0900

    기본 제공 코드들 주석 달기, StoryBoard 삭제 및 UI View 생성

--- WhatsInMyStorage/AppDelegate.swift
+++ WhatsInMyStorage/AppDelegate.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
+/*
+ * AppDelegate.swift의 역할
+    - App의 entry Point
+    - application level의 life-cycle을 관리하는 역할
+
+ * App의 entry Point
+    - Swift 프로젝트에는 왜 C++ 프로젝트 처럼 Maing 함수가 없는걸까?
+        -> UIKit 프레임워크에 숨어있기 떄문이다.
+ 
+    - Swift 컴파일러는 @main 어노테이션을 통해 AppDelegate에서 전역범위에 있는 코드를 자동으로 인식하게 하고 실행시켜 Entry Point가 되게 한다.
+ 
+ 
+ * AppDelegate Class는 UIApplicationDelegate를 채택하며, 이 프로토콜은
+    - 앱 세팅
+    - 앱 상태 변화에 응답하며 다른 app-level 이벤트를 처리하는데 사용하는 여러가지 방법을 정의합니다.
+ 
+ * iOS13 이후 부터는 Window 관리를 SceneDelegate에 위임합니다. 여러개의 Window를 사용할 수 있고, 여러개의 Scene을 사용할 수 있습니다.
+ 그렇다면 window와 scene의 차이점은 무엇일까? sceneDelegate에 설명한다.
+ */
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    /*
+     Application Setup.
+     */
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -19,12 +40,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    /*
+     Application이 새로운 Scene/Window를 제공하려고 할 때 불린다. (최초 launch 아님)
+     */
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    /*
+     Scene을 버릴 때 불린다. 버리는 상황이란 swipe로 멀티 윈도우를 없앤다던지, 코드로서 없애는 경우이다.
+     */
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- WhatsInMyStorage/SceneDelegate.swift
+++ WhatsInMyStorage/SceneDelegate.swift
@@ -7,18 +7,131 @@
 
 import UIKit
 
+/*
+ Scene/Window에 무엇을 보여줄지 관리하는 역할을 한다.
+ 
+ UIScreen
+    - UIWindowScene
+        - UIWindow
+            - UIView
+            - UIView
+ 
+    - UIWindowScene
+        - UIWindow
+            - UIView
+            - UIView
+            - UIView
+ 
+ UIWindowScene이란?
+    - UIScene의 서브 클래스로, App의 하나 이상의 Window를 관리한다.
+    - 직접 생성하면 안되고, configuration Time에 원하는 UIWindowScene에 대해 명시하라고 한다. -> info.plistdml scene configuration details
+ 
+ UISceneSession
+    - App의 Scene들 중 하나의 Scene에 대한 정보를 갖고 있는 객체
+    - 내부에 Scene: UIScene? 옵셔널 변수를 갖고, connect/disconnect 시킨다.
+ 
+ UIWindow
+    - App의 UI에 대한 배경과 Event를 View에 전달하는 객체
+    - App의 컨텐츠를 표기할 기본 Window 제공
+    - 추가 컨텐츠를 표기할 때, 필요한 경우 추가 Window를 생성한다.
+ 
+ 결론
+    - UIWindowScene 객체를 이용하여 앱의 UI를 관리한다.
+    - Scene은 여러개의 Window들과 VC들을 포함하며 하나의 UI 인스턴스를 표현한다.
+    - 각 Scene은 UIWindowSceneDelegate 객체를 갖고 있다. (각 Scene memory를 동기화한다.)
+ */
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
+    /*
+     UISceneSession lifeCycle에서 제일 처음 불리는 메서드.
+     첫 contentView인 UIWindow를 생성하고, window의 rootViewController를 설정합니다.
+     
+     이 때 헷갈리지 않아야할 점은...
+     이 'window'가 사용자가 보는 window가 아니라 app이 작동하는 viewPort라는 것.
+     
+     첫 view를 만들 때도 쓰이지만, 과거에 disconnected된 UI를 되돌릴 때도 쓰입니다.
+     
+     코드로 UI를 짜려고 스토리보드를 해제했다면, 여기서 setUp하면 된다.
+     
+     멀티 윈도우를 지원하기 때문에, UIWindowScene을 생성하면, UISceneSession으로 willConnect 되는 것이다!
+     */
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        /*
+         UIScreen
+            - UIWindowScene
+                - UIWindow
+                    - UIView
+                    - UIView
+         */
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let mainViewController = ViewController()
+        
+        self.window?.rootViewController = mainViewController
+        self.window?.makeKeyAndVisible()
     }
-
+    
+    /*
+     위 과정이 끝나면 다음 불리는 메서드.
+     
+     scene이 foreground로 전환될 때 불립니다.
+        1) background -> foreground가 되었을 때.
+        2) 처음 active 되었을 때.
+     */
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+    
+    /*
+     1) 처음 Active 되었을 때 Scene이 SetUp되고 화면에 보여지면서 사용 될 준비가 완료 된 상태.
+     2) inActive -> active로 전환할 때
+     
+     inActive 상태가 되어서 멈췄던 task를 재실행할 수도 있고, 처음 불렸다면 그냥 사용하면 된다!
+     */
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+    
+    /*
+     active -> inActive 상태로 빠질 때 불리는 메서드.
+     사용 중 전화가 걸려오는 것 처럼 임시 interruption 때문에 발생할 수 있다.
+     */
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+    
+    /*
+     scene이 foreground -> background 상태가 될 때 불리므로 다시 foreground로 돌아올 때 복원할 수 있도록
+        - state 정보를 저장하거나,
+        - 데이터를 저장하거나,
+        - 공유 자원을 돌려주는 등
+     의 작업을 하면 된다.
+     */
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+    
+    /*
+     Scene이 Background로 들어갔을 때 시스템이 자원을 확보하기 위해 disconnect하려고 할 수도 있습니다.
+     (disconnect와 app 종료는 다른 의미입니다. scene이 이 메서드로 전달되면 session에서 끊어진다는 것 뿐입니다.)
+     
+     이 메서드에서 해야할 가장 중요한 작업은 필요없는 자원을 돌려주는 것 입니다.
+        - 디스크나 네트워크를 통해 쉽게 불러올 수 있거나 생성이 쉬운 데이터는 돌려주고
+        - 사용자의 input과 같이 재생성이 어려운 데이터는 갖고 있게끔 하는 작업을 해주어야 한다.
+     
+     */
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -26,26 +139,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
+
+
 
 
 }
